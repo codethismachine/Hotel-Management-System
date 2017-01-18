04ef47a82e9fd1316b9247c2e5ea06f0694bf651
codethismachine <jasonalextatum1212@gmail.com>
2017-01-18 10:55:48 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -10,28 +10,30 @@ package hotel_management;
  * @author Jason
  */
 public class Employee {
-    private String firstName;
-    private String lastName;
-    private String dateOfHire;
-    private String department;
-    private int employeeID;
-    private String username;
-    private String password;
+    private final int EMPLOYEE_ID;
+    private final String FIRST_NAME;
+    private final String LAST_NAME;
+    private final String HIRE_YEAR;
+    private final String HIRE_MONTH;
+    private final String HIRE_DAY;
+    private final String DEPARTMENT_ID;
+    private final String USERNAME;
+    private final String PASSWORD;
     
-  /*private String firstName;
-    private String lastName;
-    private String hireYear;
-    private String hireMonth;
-    private String hireDay;
-    private String departmentID;
-    private int employeeID;
-    private String username;
-    private String password;*/
+
     
-    public Employee(String firstName, String lastName,
-        String dateOfHire, String department, int employeeID,
-        String username, String password){
+    public Employee(int EMPLOYEE_ID, String FIRST_NAME, String LAST_NAME,
+            String HIRE_YEAR, String HIRE_MONTH, String HIRE_DAY, 
+            String DEPARTMENT_ID, String USERNAME, String PASSWORD){
         
-        
+        this.EMPLOYEE_ID = EMPLOYEE_ID;
+        this.FIRST_NAME = FIRST_NAME;
+        this.LAST_NAME = LAST_NAME;
+        this.HIRE_YEAR = HIRE_YEAR;
+        this.HIRE_MONTH = HIRE_MONTH;
+        this.HIRE_DAY = HIRE_DAY;
+        this.DEPARTMENT_ID = DEPARTMENT_ID;
+        this.USERNAME = USERNAME;
+        this.PASSWORD = PASSWORD;
     }
 }
